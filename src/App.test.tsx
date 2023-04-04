1c00e4b5af923782ddf99bd113c96f084e851aef
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-04 17:03:46 -0400

    add exmaple

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import Home from './pages/Home';
 import { IonApp } from '@ionic/react';
+
+beforeEach(() => {
+  jest.spyOn(window, 'requestAnimationFrame').mockImplementation(cb => cb());
+});
+
+afterEach(() => {
+  window.requestAnimationFrame.mockRestore();
+});
 
 test('renders without crashing', async () => {
   const { baseElement } = render(<IonApp><Home /></IonApp>);
   expect(baseElement).toBeDefined();
   
   const button = screen.getByTestId('open-modal-button');
-  await button.click();
+  
+  await act(async () => {
+    await button.click();
+  });
   
   const modal = screen.getByTestId('modal-element');
-  console.log(modal)
+  console.log(modal.innerHTML)
 });
